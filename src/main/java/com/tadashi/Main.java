be895a1fd5c26c4f250899a4213067ca21b99d09
Sagar More <animsh.more@gmail.com>
2023-09-09 14:43:16 +0530

    JSON response using class

--- src/main/java/com/tadashi/Main.java
+++ src/main/java/com/tadashi/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -17,6 +19,37 @@ public class Main {
         return new GreetResponse("<h1>Hey, This is sample greeting message!</h1>");
     }
 
-    record GreetResponse(String greet){
+//    record GreetResponse(String greet){
+//    }
+
+    static class GreetResponse{
+        private final String greet;
+
+        GreetResponse(String greet){
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof GreetResponse that)) return false;
+            return Objects.equals(getGreet(), that.getGreet());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getGreet());
+        }
     }
 }
