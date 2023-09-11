e8fa23133113e8bddc0d7339191a952fcb500910
Sagar More <animsh.more@gmail.com>
2023-09-11 10:03:10 +0530

    refactor code;n-tier arch, exception with response code and message

---
+++ src/main/java/com/tadashi/customer/CustomerDataAccessService.java
@@ -0,0 +1,42 @@
+package com.tadashi.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CustomerDataAccessService implements  CustomerDao{
+
+    private static final List<Customer> customers;
+
+    static {
+        customers = new ArrayList<>();
+        Customer alex = new Customer(
+                1,
+                "Alex",
+                "alex@gmail.com",
+                23
+        );
+
+        Customer sam = new Customer(
+                2,
+                "Sam",
+                "sam@gmail.com",
+                23
+        );
+        customers.add(alex);
+        customers.add(sam);
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return customers;
+    }
+
+    @Override
+    public Optional<Customer> getCustomerById(int id) {
+        return customers.stream().filter(c -> c.getId() == id).findFirst();
+    }
+}
